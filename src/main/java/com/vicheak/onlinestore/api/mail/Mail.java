f702803dc3c6dcca6ac4efb6ddb4d89d6e54f76b
Vicheak <suonvicheak991@gmail.com>
2023-10-26 20:04:33 +0700

    implement template for mail

---
+++ src/main/java/com/vicheak/onlinestore/api/mail/Mail.java
@@ -0,0 +1,14 @@
+package com.vicheak.onlinestore.api.mail;
+
+import lombok.Data;
+
+@Data
+public class Mail<T> {
+
+    private String sender;
+    private String receiver;
+    private String subject;
+    private String template;
+    private T metaData;
+
+}
